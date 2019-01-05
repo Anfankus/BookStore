fea88f5f7813cf35fe276ae5f9b314ae2b2e5e04
anfanger <zhangke.my.186@gmail.com>
2019-01-05 23:12:16 +0800

    test. finished

--- server/database.js
+++ server/database.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
-    password: '123456',
+    password: '',
     port: '3306',
     database: 'BookStore'
 });
