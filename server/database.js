827c0489161c8715a3840e9fdb58fcc2b3784493
李嘉兴 <Leejiaxing@Handsome.local>
2019-01-05 21:46:38 +0800

    update. adjust order procedure

--- server/database.js
+++ server/database.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
-    password: '',
+    password: '123456',
     port: '3306',
     database: 'BookStore'
 });
