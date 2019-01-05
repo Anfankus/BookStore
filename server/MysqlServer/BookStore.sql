ebbe342ab9817570a60f15af6dd405cc6ea7afe2
anfanger <zhangke.my.186@gmail.com>
2019-01-06 00:18:25 +0800

    fix. sql file insert statement update

--- server/MysqlServer/BookStore.sql
+++ server/MysqlServer/BookStore.sql
@@ -18,6 +18,7 @@ SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 CREATE DATABASE `BookStore`;
+USE BookStore;
 -- ----------------------------
 -- Table structure for bookInfo
 -- ----------------------------
